839c28fcf2a0f83bec6e7173fc0446a6e60faf6a
Ken Johnson <k.rjohnson94@outlook.com>
2025-01-02 23:02:16 -0500

    Add GameManager, PlayroomManager, and CinemaController

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,25 @@
-using Playroom;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class GameManager : MonoBehaviour
 {
-    private PlayroomKit _roomKit = new();
+    [Header("Scene Indices")]
+
+    
+    [SerializeField] public int RouletteCutsceneIndex;
+    [SerializeField] public int RouletteTableSceneIndex;
+    [SerializeField] public int SlotMachineCutsceneIndex;
+    [SerializeField] public int SlotMachineSceneIndex;
+    [SerializeField] public int CrapsTableCutsceneIndex;
+    [SerializeField] public int CrapsTableSceneIndex;
+
+    [Header("Game Mechanics")]
+    [SerializeField] public float InitialDifficulty;
+    [SerializeField, Range(1f, 10f)] public float DifficultyScale;
+
+    private float _defaultTransitionTime = 3.0f;
 
     // Called when MonoBehaviour is first instantiated before Start()
     void Awake()
@@ -23,5 +38,21 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public IEnumerator LoadLevelWithDelay(int index, int delayInSeconds)
+    {
+        Debug.Log($"Scene requested - id: {index}");
+        //TODO: Do we need a transition?
+        yield return new WaitForSeconds(delayInSeconds);
+
+        SceneManager.LoadScene(index);
+    }
+
+    public void LoadLevelImmediate(int index)
+    {
+        Debug.Log($"Scene requested immediately - id: {index}");
+        
+        SceneManager.LoadScene(index);
     }
 }
