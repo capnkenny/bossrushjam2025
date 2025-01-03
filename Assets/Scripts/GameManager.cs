aa5224a854032ff696f1ea93df4ef80a5edf304c
Ken Johnson <k.rjohnson94@outlook.com>
2025-01-02 22:02:41 -0500

    Change project settings for better testing of WebGL

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,18 @@
+using Playroom;
 using UnityEngine;
+
 
 public class GameManager : MonoBehaviour
 {
+    private PlayroomKit _roomKit = new();
+
+    // Called when MonoBehaviour is first instantiated before Start()
+    void Awake()
+    {
+        //Don't get used to this pls.
+        DontDestroyOnLoad(gameObject);    
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
