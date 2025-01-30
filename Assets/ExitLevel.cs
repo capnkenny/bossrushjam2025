27371a7cb4aa3bf80b4b509b24d5cda902b3efa2
Ken Johnson <k.rjohnson94@outlook.com>
2025-01-30 16:02:38 -0500

    everything but game over

--- Assets/ExitLevel.cs
+++ Assets/ExitLevel.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class ExitLevel : MonoBehaviour
 {
     public WarpToLevel warpSpot;
-    
+
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
