52d47d45999b64c2ec08fd06c6b6d21544670ba2
Ken Johnson <k.rjohnson94@outlook.com>
2025-02-01 16:03:36 -0500

    Fix chip logic to die on chipdestroyer impact

--- Assets/ChipDestroyer.cs
+++ Assets/ChipDestroyer.cs
@@ -4,6 +4,8 @@ public class ChipDestroyer : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D c)
     {
+        Debug.Log("col: "+c.gameObject.name);
+
         if(c.gameObject.tag == "Projectile" || c.gameObject.tag == "PlayerProjectile")
         {
             Destroy(c.gameObject);
