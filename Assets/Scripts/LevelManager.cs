a3552a2c326369394c365ec8cddd59ae7daa6b5d
Ken Johnson <k.rjohnson94@outlook.com>
2025-02-02 11:16:46 -0500

    Add lvl mgr for lobby and redirect poker to game over screen

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,16 +1,31 @@
+using System.Linq;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    private GameManager gameManager;
+    public LockableBehaviour Roulette;
+    public LockableBehaviour Poker;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        var list = FindObjectsByType<GameManager>(FindObjectsSortMode.None);
+        if (list != null && list.Length != 0)
+        {
+            gameManager = (GameManager)list.First();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(gameManager)
+        {
+            if(Roulette)
+                Roulette.Locked = gameManager.LevelOneComplete ? false : true;
+            if(Poker)
+                Poker.Locked = gameManager.LevelTwoComplete ? false : true;
+        }
     }
 }
