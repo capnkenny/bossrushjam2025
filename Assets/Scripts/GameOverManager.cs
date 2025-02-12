9afc7988ab92b4737df825d06cd3e5aeceb5ec26
Ken Johnson <k.rjohnson94@outlook.com>
2025-02-11 22:56:42 -0500

    Sync main with bossrushjam demo
    Gets mainline in sync with what was published last minute to boss rush jam due to technical difficulties

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -8,6 +8,10 @@ public class GameOverManager : MonoBehaviour
     public GameManager gameManager;
     [SerializeField] private TextMeshProUGUI toBeContinuedText;
 
+    public TextMeshProUGUI thanksText;
+
+    public TextMeshProUGUI creditsText;
+
     void Start()
     {
         var list = FindObjectsByType<GameManager>(FindObjectsSortMode.None);
@@ -16,6 +20,19 @@ public class GameOverManager : MonoBehaviour
             gameManager = (GameManager)list.First();
         }
         toBeContinuedText.gameObject.SetActive(true);
+        StartCoroutine(ShowThanksForPlaying());
+    }
+
+    public IEnumerator ShowThanksForPlaying()
+    {
+        yield return new WaitForSeconds(3.0f);
+        thanksText.gameObject.SetActive(true);
+        toBeContinuedText.gameObject.SetActive(false);
+        yield return new WaitForSeconds(3.5f);
+        thanksText.gameObject.SetActive(false);
+        creditsText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(6.0f);
+        gameManager.LoadLevelImmediate(2);
     }
 
 }
